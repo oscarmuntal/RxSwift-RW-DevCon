6dd86278f1b049a488307575d35b0094ae596c0c
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-17 19:12:48 +0100

    RxSwift Playground

--- RxSwift RW DevCon/RxSwift.playground/Contents.swift
+++ RxSwift RW DevCon/RxSwift.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+import PlaygroundSupport
+import RxSwift
+import RxCocoa
 
-var greeting = "Hello, playground"
+let names = BehaviorRelay<[String]>(value: [])
+
+names.asObservable()
+    .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+    .filter { value in
+        return value.count > 1
+    }
+//    .debug()
+    .map { value in
+        return "users: \(value)"
+    }
+//    .debug()
+    .subscribe(onNext: { value in
+        print(value)
+    })
+
+names.accept(["Peter"])
+names.accept(["Peter", "John"])
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+    names.accept(["Claudia", "Alice"])
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+    names.accept(["Admin"])
+}
