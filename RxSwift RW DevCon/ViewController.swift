618be07dc4f69b67acb09983fac44d644af03193
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-17 13:31:23 +0100

    Initial Commit

---
+++ RxSwift RW DevCon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwift RW DevCon
+//
+//  Created by Òscar Muntal on 17/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
