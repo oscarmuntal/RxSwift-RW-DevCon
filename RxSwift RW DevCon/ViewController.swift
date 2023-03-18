e02ca7831d787dddd4b3a9c48ed70581ea9705fd
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-18 21:10:49 +0100

    Starting Demo2 ViewController which will simulate downloading a stocks list and then allow to filter them

--- RxSwift RW DevCon/ViewController.swift
+++ RxSwift RW DevCon/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    fileprivate let bag = DisposeBag()
+    fileprivate let allSymbols = ["RZW", "UDP", "MTT", "ZKQ", "IPK", "AQÜ"]
+    fileprivate let allPrices = BehaviorRelay<[StockPrice]>(value: [])
+    fileprivate let prices = BehaviorRelay<[StockPrice]>(value: [])
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var favoritesSwitch: UISwitch!
+    @IBOutlet var searchTerm: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
 
+struct StockPrice {
+    
+}
